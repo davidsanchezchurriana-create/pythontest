d734507509cd4220aef527bb5256ad80a68a6654
David Sánchez <davidsanchezchurriana@gmail.com>
2026-09-23 14:42:51 +0200

    Comienzo del ejercicio 9 (a terminar)

---
+++ Tipos de datos simples/ej9.py
@@ -0,0 +1,5 @@
+inversion=input("Introduce la cantidad a invertir: ")
+interes_anual=input("Introduce el interes anual: ")
+num_años=("Introduce el numero de años: ")
+# Continuar aqui con el calculo:
+# capital=inversion*(1+interes_anual/100)**num_años
