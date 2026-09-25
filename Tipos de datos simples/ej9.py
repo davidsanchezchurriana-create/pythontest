150b81c2a97ea46302cd600f4fcf0afa077e7c87
David Sánchez <davidsanchezchurriana@gmail.com>
2026-09-25 13:51:44 +0200

    9º ejercicio terminado "Tipos de datos simples"

--- Tipos de datos simples/ej9.py
+++ Tipos de datos simples/ej9.py
@@ -1,5 +1,5 @@
-inversion=input("Introduce la cantidad a invertir: ")
-interes_anual=input("Introduce el interes anual: ")
-num_años=("Introduce el numero de años: ")
-# Continuar aqui con el calculo:
-# capital=inversion*(1+interes_anual/100)**num_años
+inversion=float(input("Introduce la cantidad a invertir: "))
+interes_anual=float(input("Introduce el interes anual: "))
+num_años=float(input("Introduce el numero de años: "))
+capital=float(inversion*(1+interes_anual/100)**num_años)
+print(f"Tu capital obtenido en tu inversion es de {capital}€")
